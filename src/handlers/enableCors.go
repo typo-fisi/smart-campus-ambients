1cb217c4ca1a45f538791a2a34d1f4791cdcb3fe
PandaDiestro <destructo900@gmail.com>
2023-08-03 14:31:30 -0500

    cors test

--- src/handlers/enableCors.go
+++ src/handlers/enableCors.go
@@ -4,7 +4,6 @@ import "net/http"
 
 func EnableCors(w *http.ResponseWriter) {
     (*w).Header().Add("Access-Control-Allow-Origin", "*")
-    (*w).WriteHeader(200);
     (*w).Header().Set("Access-Control-Allow-Origin", "*")
     (*w).Header().Set("Access-Control-Allow-Methods", "*")
     (*w).Header().Set("Access-Control-Allow-Headers", "*")
